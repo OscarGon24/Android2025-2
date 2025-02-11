db2976b08a2059814adf31285cfbf633686fece7
OscarGon24 <oscargonzlez2412@gmail.com>
2025-02-11 16:15:51 -0600

    Mi primer calculadora

---
+++ 10-02-2025/CalculadoraImpl.java
@@ -0,0 +1,11 @@
+class CalculadoraImpl implements ICalculadora {
+
+	public Double calcular(Operacion operacion){
+		switch (operacion.getOperando()) {
+			case SUMA:
+				return operacion.getX1() + operacion.getX2();
+			default:
+				return 0.0;
+		}
+	}
+}
---
+++ 10-02-2025/ICalculadora.java
@@ -0,0 +1,4 @@
+interface ICalculadora {
+
+	Double calcular(Operacion operacion);
+}
